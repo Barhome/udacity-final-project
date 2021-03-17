a3c422fc679413a1c1d09c7204e0241295a772dc
barhome <barhome@github.com>
2021-03-17 07:37:47 +0200

    feat:Creating an attractive intro section

--- src/client/index.js
+++ src/client/index.js
@@ -6,8 +6,13 @@ import "./styles/header.scss";
 import "./styles/grid.scss";
 import "./styles/base.scss";
 import imgStart from "./media/images/1.jpg";
+import imgIntroJs from "./media/images/1.png";
+
 const img = document.getElementById("city-image");
+const imgIntro = document.getElementById("img-intro");
+
 img.src = imgStart;
+imgIntro.src = imgIntroJs;
 document.getElementById("save-trip").addEventListener("click", handleInputs);
 
 export { imgStart };
